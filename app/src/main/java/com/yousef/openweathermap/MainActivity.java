6b2923db2c49af69b06dccd4d627dc9c658a6ae0
Yousef <neuer.youseftaheri@gmail.com>
2021-06-05 03:22:11 +0430

    Implement an API request & update UI using Retrofit and RxJava

--- app/src/main/java/com/yousef/openweathermap/MainActivity.java
+++ app/src/main/java/com/yousef/openweathermap/MainActivity.java
@@ -12,11 +12,18 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.yousef.openweathermap.data.api.ApiAdapter;
+import com.yousef.openweathermap.data.api.ApiService;
+import com.yousef.openweathermap.data.api.rx.RxApiCallHelper;
+import com.yousef.openweathermap.data.api.rx.RxApiCallback;
 import com.yousef.openweathermap.data.model.CityResponse;
 import com.yousef.openweathermap.data.model.JsonData;
+import com.yousef.openweathermap.data.model.TemperatureResponse;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tvTemperature)
     TextView tvTemperature;
     @BindView(R.id.btSubmit)
-    TextView btSubmit;
+    Button btSubmit;
     @BindView(R.id.etCity)
     TextView etCity;
     @BindView(R.id.etRank)
     TextView etRank;
+    @BindView(R.id.tilCity)
+    TextInputLayout tilCity;
+    @BindView(R.id.tilRank)
+    TextInputLayout tilRank;
     private ProgressDialog mProgressDialog;
     private Disposable disposable = null;
     private GpsTracker gpsTracker;
@@ -71,8 +82,22 @@ public class MainActivity extends AppCompatActivity {
             Log.d("city name: ", city.getName());
             Log.d("city location lat: ", Double.toString(city.getLocation().getLat()));
         }
+
         btSubmit.setOnClickListener(v -> {
-            updateInfo();
+            if(etCity.getText().toString().isEmpty())
+                tilCity.setError(getString(R.string.empty_city));
+            else if(etRank.getText().toString().isEmpty())
+                tilRank.setError(getString(R.string.empty_rank));
+            else {
+                tilRank.setError(null);
+                tilCity.setError(null);
+                updateInfo();
+                if (isInternetAvailable()) {
+                    getTemperature();
+                } else {
+                    showToast(getString(R.string.no_internet_connection));
+                }
+            }
         });
     }
 
@@ -90,8 +115,8 @@ public class MainActivity extends AppCompatActivity {
         if(gpsTracker.canGetLocation()){
             double latitude = gpsTracker.getLatitude();
             double longitude = gpsTracker.getLongitude();
-            tvLatitude.setText(String.valueOf(latitude));
-            tvLongitude.setText(String.valueOf(longitude));
+            tvLatitude.setText(String.format("%.5f", latitude));
+            tvLongitude.setText(String.format("%.5f", longitude));
             CityResponse.Location location = new CityResponse.Location();
             location.setLat(latitude);
             location.setLon(longitude);
@@ -99,5 +124,57 @@ public class MainActivity extends AppCompatActivity {
         }else{
             gpsTracker.showSettingsAlert();
         }
+    }
+
+    private void getTemperature() {
+        mProgressDialog.show();
+        ApiService apiService = ApiAdapter.getInstance(this).provideService(ApiService.class);
+        Observable<TemperatureResponse> responseObservable =
+                apiService.getCityTemperature(etCity.getText().toString());
+        disposable = RxApiCallHelper.call(responseObservable, new RxApiCallback<TemperatureResponse>() {
+            @Override
+            public void onSuccess(TemperatureResponse temperatureResponse) {
+                mProgressDialog.dismiss();
+                if(temperatureResponse != null) {
+                    showToast(temperatureResponse.getMain() != null ? "Success" : "Failed");
+                    if (temperatureResponse.getMain() != null) {
+                        showTemperature(temperatureResponse);
+                    }
+                }else {
+                    showToast("Something went wrong");
+                }
+                disposeCall();
+            }
+
+            @Override
+            public void onFailed(Throwable throwable) {
+                disposeCall();
+                mProgressDialog.dismiss();
+                showToast(throwable.getLocalizedMessage());
+            }
+        });
+    }
+
+    private void disposeCall() {
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void showTemperature(TemperatureResponse temperatureResponse) {
+        tvTemperature.setText(String.format("%.2f", (temperatureResponse.main.temp - 273.15F)) + " °C");
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean isInternetAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting()
+                && cm.getActiveNetworkInfo().isAvailable()
+                && cm.getActiveNetworkInfo().isConnected();
     }
 }
